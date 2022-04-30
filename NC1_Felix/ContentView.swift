484396811fc68593748a524794a59f9879cbaa03
Pos-Felix <wonyang22@pos.idserve.net>
2022-04-30 13:56:01 +0900

    Initial Commit

---
+++ NC1_Felix/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NC1_Felix
+//
+//  Created by 양원모 on 2022/04/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NC1_Felix/NC1_FelixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1_FelixApp.swift
+//  NC1_Felix
+//
+//  Created by 양원모 on 2022/04/30.
+//
+
+import SwiftUI
+
+@main
+struct NC1_FelixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
